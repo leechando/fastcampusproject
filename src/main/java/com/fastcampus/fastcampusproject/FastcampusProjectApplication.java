ea7e3b4f05808949b04c5eb4f496ac2c9c24287b
chando <dlckseh1@naver.com>
2022-11-07 16:49:49 +0900

    test

---
+++ src/main/java/com/fastcampus/fastcampusproject/FastcampusProjectApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.fastcampusproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastcampusProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastcampusProjectApplication.class, args);
+    }
+
+}
